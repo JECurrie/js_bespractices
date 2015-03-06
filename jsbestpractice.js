fd1597c132655a71cab291b29c923c2721e8cd13
Joan Currie <jecurrie8@gmail.com>
2015-03-06 12:07:17 -0500

    ternary

---
+++ jsbestpractice.js
@@ -0,0 +1,5 @@
+var allPacked = false;
+var adventureTime;
+
+adventureTime = allPacked ? "yes" : "no" ;
+console.log("The above ternary conditional will assign " + adventureTime + " to the adventureTime variable.");
